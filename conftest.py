db1657335698997164352f42b93e893aeb97d2c7
andrewgluss <bangluss@gmail.com>
2022-11-15 11:34:26 +0300

    add choice of langauges

--- conftest.py
+++ conftest.py
@@ -1,23 +1,32 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                  help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="en",
+                 help="Choose language: en, ru, fr, es, ...")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
     browser = None
-    if browser_name == "chrome":
+    if browser_name == "chrome" or None:
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    #else:
+        #raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
     browser.quit()
