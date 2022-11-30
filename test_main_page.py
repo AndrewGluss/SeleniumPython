501e7b9185a0baf47ae21eb2fe60ad2d014bba59
andrewgluss <bangluss@gmail.com>
2022-11-30 16:19:00 +0300

    create test using Page Object Psttern

---
+++ test_main_page.py
@@ -0,0 +1,7 @@
+from .page_object.main_page import MainPage
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выпол yяем метод страницы — переходим на страницу логина
